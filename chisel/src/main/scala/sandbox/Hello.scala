05c3654154c2fa503e618914888bd9657f28dc7b
Jack Davine <jackdavine47@gmail.com>
2024-04-30 17:43:25 +0800

    add Pipe module

--- chisel/src/main/scala/sandbox/Hello.scala
+++ chisel/src/main/scala/sandbox/Hello.scala
@@ -1,13 +1,18 @@
 package sandbox
 
 import chisel3._
+import chisel3.util.Pipe
 import org.chipsalliance.cde.config.Parameters
 
 class Hello()(implicit p:Parameters) extends Module {
 
   val io = IO(new Bundle {
-    val out = Output(UInt(10.W))
+    val in = Input(UInt(8.W))
+    val out = Output(UInt(8.W))
   })
+  val pipe = Module(new Pipe(UInt(8.W),40))
   println(io.out.getWidth)
-  io.out := 42.U
+  pipe.io.enq.bits := io.in
+  pipe.io.enq.valid := true.B
+  io.out := pipe.io.deq.bits
 }
