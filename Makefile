66d3f6fe7610ffb20428efbb229992b64174d112
Jack Davine <jackdavine47@gmail.com>
2024-12-25 16:52:45 +0800

    remove dead codes

--- Makefile
+++ Makefile
@@ -2,29 +2,9 @@ test:
 	mill emulator[sandbox.Hello,sandbox.HelloConfig,DiplomacyGenerator].mfccompiler.compile
 test1:
 	mill emulator[examples.Adder,sandbox.HelloConfig,SimpleGenerator].mfccompiler.compile
-v1-adder:
-	mill -i changbaiV1.spinal.runMain v1.AdderExample
-
-v1-regfile:
-	mill -i changbaiV1.spinal.runMain v1.RegFileExample
-
-v1-decoder:
-	mill -i changbaiV1.spinal.test.runMain changbaiV1.v1.test.IDecoder
-
-v1-alu:
-	mill -i changbaiV1.spinal.test.runMain changbaiV1.v1.test.ALU
-
-v1-print:
-	mill -i changbaiV1.spinal.test.runMain changbaiV1.v1.test.Print
-
-v1-play:
-	mill -i changbaiV1.spinal.test.runMain changbaiV1.v1.test.Play
 
 hello:
 	mill -i changbaiV1.spinal.runMain changbaiV1.sayHello
-
-changbai:
-	mill -i changbaiV1.spinal.runMain changbaiV1.genChangbai
 
 idea:
 	mill mill.idea.GenIdea/idea
--- spinal/src/test/scala/genTestChangbai.scala
+++ spinal/src/test/scala/genTestChangbai.scala
@@ -1,45 +1,6 @@
 package changbaiV1.v1.test
 
 import spinal.core.{SpinalConfig, SystemVerilog}
-import v1.IDecode
-import v1.fn.{ALUFull, ALULite, ALUSum}
-import v1.pipe.SimpleAluRs
-
-object ALU extends App{
-  println("Gen ChangBai ALU......")
-  val sc = SpinalConfig(mode = SystemVerilog, targetDirectory = "changbaiTest", genLineComments = true, oneFilePerComponent = true)
-  sc.generate {
-    val topLevel = new ALUFull
-    topLevel
-  }
-  sc.generate{
-    val topLevel = new ALUSum
-    topLevel
-  }
-  sc.generate {
-    val topLevel = new ALULite
-    topLevel
-  }
-}
-object IDecoder extends App{
-  println("Gen ChangBai IDecoder......")
-  SpinalConfig(mode = SystemVerilog, targetDirectory = "changbaiTest", genLineComments = true, oneFilePerComponent = true)
-    .generate {
-      val topLevel = new IDecode
-      topLevel
-    }
-}
-
-object SimpleRs {
-  def main(args: Array[String]) {
-    println("test Gen SimpleRs......")
-    SpinalConfig(mode = SystemVerilog, targetDirectory = "changbaiTest", genLineComments = false, oneFilePerComponent = true)
-      .generate {
-        val topLevel = new SimpleAluRs()
-        topLevel
-      }
-  }
-}
 
 object Play {
   def main(args: Array[String]) {
