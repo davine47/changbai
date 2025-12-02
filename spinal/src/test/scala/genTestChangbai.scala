d88d8408e4f527da74b4f4c9ed4dc905a5c617a8
davine47 <jackdavine47@gmail.com>
2025-12-02 14:22:26 +0800

    update verilog generation

--- spinal/src/test/scala/genTestChangbai.scala
+++ spinal/src/test/scala/genTestChangbai.scala
@@ -5,7 +5,13 @@ import spinal.core.{SpinalConfig, SystemVerilog}
 object Play {
   def main(args: Array[String]) {
     println("test Gen Misc......")
-    SpinalConfig(mode = SystemVerilog, targetDirectory = "rtl", genLineComments = true, oneFilePerComponent = true)
+    SpinalConfig(
+      mode = SystemVerilog,
+      targetDirectory = "rtl",
+      genLineComments = true,
+      oneFilePerComponent = true,
+      withTimescale = false,
+      printFilelist = false)
       .generate {
         val topLevel = new MyPlay
         topLevel
